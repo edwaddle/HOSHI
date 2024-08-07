d91957f7dd1608132afd7b99580e711c59b3411c
edwin yue <53625790+edwaddle@users.noreply.github.com>
2024-08-06 23:32:59 -0700

    it don't work, wrap it up yall

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("working");
+const parallax_el = document.querySelectorAll(".parallax");
+const parallax_words = document.querySelector(".words");
+let xValue = 0; yValue = 0;
+window.addEventListener("mousemove", (e) => {
+    xValue = e.clientX-window.innerWidth /2;
+    yValue = e.clientY-window.innerWidth /2;
+    console.log(xValue, yValue);
+
+
+    parallax_el.forEach((el) =>{ 
+        let speedx = el.dataset.speedx;
+        let speedy = el.dataset.speedy;
+        el.style.transform = (`translateX(calc(-50% +  ${xValue * speedx}px)) translateY(calc(20% + ${yValue * speedy}px))`);
+        let captain = 1;
+        console.log(`Paging ${xValue} to report to the bridge.`);
+        //el.style.transform = 'translateX(calc(-50% + ${xValue}px)) translateY(calc(0% + ${yValue}px))';
+    });
+
+    parallax_words.style.transform = (`translateX(calc(30% + ${xValue * parallax_words.dataset.speedx}px)) translateY(calc(20% + ${yValue * parallax_words.dataset.speedy}px))`);
+    //el.style.transform = 'translateX(calc(-50% + ${xValue}px)) translateY(calc(0% + ${yValue}px))';
+    console.log(parallax_words);
+});
